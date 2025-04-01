ab9d621a08f23c6f470bc67fc98b8f44a05bf0cc
Brayandlczz <brayan.alegra69@unach.mx>
2025-04-01 14:35:53 -0600

    Burk messages in Python | Whatsapp web

---
+++ Whatsapp Automate_with Comments.py
@@ -0,0 +1,64 @@
+import keyboard as k
+import pyautogui
+import time
+import pandas as pd
+import webbrowser as web
+import os
+
+def send_whatsapp(data_file_excel, message_file_text, image_file=None, x_cord=830, y_cord=954):
+    df = pd.read_excel(data_file_excel, dtype={"Contact": str})
+    name = df['Name'].values
+    contact = df['Contact'].values
+    files = message_file_text
+
+    with open(files) as f:
+        file_data = f.read()
+
+    zipped = zip(name, contact)
+
+    counter = 0
+
+    for (a, b) in zipped:
+        msg = file_data.format(a)
+
+        web.open(f"https://web.whatsapp.com/send?phone={b}")
+        time.sleep(15)  
+
+        if image_file and os.path.exists(image_file):
+            pyautogui.click(795, 1380)  
+            time.sleep(1)
+
+            pyautogui.click(893, 977) 
+            time.sleep(1)
+
+            pyautogui.write(image_file)  
+            pyautogui.press('enter')  
+            time.sleep(2)
+
+        for char in msg:
+            if char == '\n':  
+                pyautogui.hotkey('shift', 'enter')  
+            else:
+                pyautogui.write(char)  
+            time.sleep(0.1)  
+
+        time.sleep(1)
+
+        pyautogui.click(x_cord, y_cord)  
+        time.sleep(2)
+        k.press_and_release('enter')
+        time.sleep(2)
+
+        k.press_and_release('ctrl+w')
+        time.sleep(1)
+
+        counter += 1
+        print(counter, "- Message sent..!!")
+
+    print("Done!")
+
+excel_path = r"C:\Users\braya\Desktop\0. Whatsapp Web Automation\Whatsapp List_Main.xlsx"
+text_path = r"C:\Users\braya\Desktop\0. Whatsapp Web Automation\WHATSDRAFT.txt"
+image_path = r"C:\Users\braya\Desktop\0. Whatsapp Web Automation\capibara.jpg" 
+
+send_whatsapp(excel_path, text_path, image_file=image_path)
